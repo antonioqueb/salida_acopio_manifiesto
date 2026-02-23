6dff7601ee6cac07c311b19ee8989c5e8afad1f8
Antonio Queb <cardstillo@gmail.com>
2026-02-23 12:20:20 -0600

    Actualiza dependencia de ingreso_acopio a ingreso_acopio_sai en el manifiesto

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,7 @@
     ''',
     'author': 'Alphaqueb Consulting',
     'website': 'https://alphaqueb.com',
-    'depends': ['base', 'stock', 'manifiesto_ambiental', 'ingreso_acopio'],
+    'depends': ['base', 'stock', 'manifiesto_ambiental', 'ingreso_acopio_sai'],
     'data': [
         'security/ir.model.access.csv',
         'data/stock_data.xml',
